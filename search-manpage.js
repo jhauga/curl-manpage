46eeb0ce392c59343a0d3db9e00eb86c9539378e
jhauga <johnhaugabook@gmail.com>
2023-08-01 18:39:26 -0400

    pre test

--- search-manpage.js
+++ search-manpage.js
@@ -23,56 +23,49 @@
 *  in this Software without prior written authorization of the copyright holder.
 **********************************************************************************/
 
-// on/off switch for top button, menu, mouseover, and button status respectively
-var setScroll = 0; var scrollOver100 = 0; var mousedOver = 0; var onOffButton = 0;
+// on/off switch for menu scroll breakpoint, mouseover, and button status respectively
+var scrollOver100 = 0; var mousedOver = 0; var onOffButton = 0;
 
 // Option menu DOM elements
-var toTop = document.getElementById("toTop"); // to top button
 var searchButton = document.getElementById("searchButton"); // show option dropdown
-var searchButtonData = searchButton.dataset; // switch for button on mouseover
 var manpageOptionMenu = document.getElementById("manpage-option-menu"); // top of DOM
 var manpageOptionMenuData = manpageOptionMenu.dataset; // switch to put menu to side
 
-// Show to top button.
-function showTop() {  
-    let curScroll, nextScroll;
-    document.addEventListener("scroll", function() {
-      // switch for scrolled down over 100
-      if (document.documentElement.scrollTop > 100) { scrollOver100 = 1; }
-      else { scrollOver100 = 0; } 
-      
-      // Toggle button to side and stay
-      if ( // scrolled past 100px, menu is not on side, and not moused over
-        scrollOver100 == 1 &&
-        manpageOptionMenuData.onSide == 0 &&
-        mousedOver == 0 && 
-        onOffButton == 0
-       ) {       
-       manpageOptionMenu.dataset.onSide = 1;
-       if ( searchButton.className.indexOf("searchButtonToSide") > -1 &&
-            manpageOptionMenuData.toSide == 1 ) {
-          // put menu to side and done          
-          searchButton.dataset.toSide = 1;
-       }
+// Show in full or put to side top button.
+function showTopButtonInFull() {
+  // Add scroll event to menu and top button.
+  document.addEventListener("scroll", function() {
+    // switch for scrolled down over 100
+    if (document.documentElement.scrollTop > 100) { scrollOver100 = 1; }
+    else { scrollOver100 = 0; } 
+
+    // Toggle button to side and stay
+    if ( // scrolled past 100px, menu is not on side, and not moused over
+      scrollOver100 == 1 &&
+      manpageOptionMenuData.onSide == 0 &&
+      mousedOver == 0 && 
+      onOffButton == 0
+     ) {       
+     manpageOptionMenu.dataset.onSide = 1;
+      if ( searchButton.className.indexOf("searchButtonToSide") > -1 &&
+           manpageOptionMenuData.toSide == 1 ) {
+        // put menu to side and done          
+        searchButton.dataset.toSide = 1;
       }
-      else if (document.documentElement.scrollTop > 500) {       
-        // show to top button on right
-        toTop.style.display = "inline-block";       
-      } else {
-       // hide to top button
-       toTop.style.display = "none"; // hide to top       
-       searchButton.dataset.toSide = 0; // button default
-       
-       // set menu to defaults
-       if (document.documentElement.scrollTop < 100) {
-         manpageOptionMenu.style.left = ""; // now use css styles
-         manpageOptionMenu.dataset.onSide = 0;
-       }
+    } else {
+      // hide top button       
+      searchButton.dataset.toSide = 0; // button default
+     
+      // set menu to defaults
+      if (document.documentElement.scrollTop < 100) {
+        manpageOptionMenu.style.left = ""; // now use css styles
+        manpageOptionMenu.dataset.onSide = 0;
       }
-    });  
+    }
+  });  
 }
 
-// Put to top and show option buttons to side when menu not in view
+// Show option buttons to side when menu not in view
 // adding mouseover function to activate.
 function buttonToSide(status, onoff) {
   // if on exit    
@@ -82,7 +75,7 @@ function buttonToSide(status, onoff) {
     ) { mousedOver = 1; return; }
   
   // is moused over or not. exit on mouse out  
-  if ( status == 1 ) { 
+  if ( status == 1 ) {
     mousedOver = 1;
   } else { 
     mousedOver = 0;
@@ -108,58 +101,58 @@ function buttonToSide(status, onoff) {
 function searchOptions(txt) {
   let optionMenu = document.getElementById("optionMenu");
   let optionMenuLi = optionMenu.getElementsByTagName("li");
-  for (i = 0; i < optionMenuLi.length; i++) {
+  // show or hide options when searching.
+  for (let i = 0; i < optionMenuLi.length; i++) {
     let curLI = optionMenuLi[i].innerText;
-    if (curLI.indexOf(txt) > -1) {
+    if (curLI.indexOf(txt) > -1) { // if match show
       optionMenuLi[i].style.display = "";
-    } else {
+    } else { // else don't show
       optionMenuLi[i].style.display = "none";
     }
   }
 }
 
-// Show and hide table of contents
+// Show and hide option list items.
 function toggleSearchButton(showhide, onoff) {
   // responsive elements
   let manpageDiv = document.getElementsByClassName("manpage")[0];
   let manpageMenu = document.getElementsByClassName("menu")[0];  
   
   // toggle display elements
-  let showItem = showhide.nextElementSibling;                  //----\ select option menu
-  let curData = onoff.dataset;                               //      |
-  if (curData.onoff == 0) {                                //        |
-    // 1.set switch, show menu, change style, change text-//---------|
-    manpageOptionMenu.dataset.toSide = 0;                //          |
-    curData.onoff = 1;                                  //           | 
-    showItem.style.display = "";                       //            |
-    onoff.style.background = "white";                  //            |
-    onoff.style.color = "black";                       //           \|/ 
-    onoff.innerHTML = onoff.innerHTML.replace("Show", "Hide"); //    .                                                              
-    // 2. set to side when menu out of view and style parrent //-----|
-    onoff.className = "";                              //            |
-    onoff.parentElement.className = onoff              //           \|/
-    .parentElement.className.replace(" inactive", ""); //          2 .    
-    // 3. responsive margins if screen < 770 add else remove //------|
-    if (!manpageDiv.id) { manpageDiv.id = "activeManDiv"; }   //    \|/
-    if (!manpageMenu.id) { manpageMenu.id = "activeManMenu"; } //  3 .    
-    // Turn button on                                         //
-    onOffButton = 1;                                         //
-  } else {                                                  //
-    // "..." -    -   -   -   -   -   -   -   -    -    - .........1 .
-    manpageOptionMenu.dataset.toSide = 1;                //         /|\
-    curData.onoff = 0;                                  //           |
-    showItem.style.display = "none";                   //            |
-    onoff.style.background = "";                       //            |
-    onoff.style.color = "";                            //            |
-    onoff.innerHTML = onoff.innerHTML.replace("Hide", "Show"); //    |
-    // "..." -    -   -   -   -   -   -   -   -    -   //..........2 .
-    onoff.className = "searchButtonToSide";            //           /|\
-    onoff.parentElement.className += " inactive";      //            |
-    // "..." -    -   -   -   -   -   -   -   -    -     //........3 .
-    if (manpageDiv.id) { manpageDiv.removeAttribute("id");} //      /|\
-    if (manpageMenu.id) { manpageMenu.removeAttribute("id");} //     |    
-    //                                                          //   |
-    // put back to side if scrolled past 100. -----------------------/
+  let showItem = showhide.nextElementSibling;
+  let curData = onoff.dataset;
+  if (curData.onoff == 0) {
+    // 1.set switch, show menu, change style, change text
+    manpageOptionMenu.dataset.toSide = 0;
+    curData.onoff = 1;
+    showItem.style.display = "";
+    onoff.style.background = "white";
+    onoff.style.color = "black";
+    onoff.innerHTML = onoff.innerHTML.replace("Show", "Hide");
+    // 2. show the option menu.
+    onoff.className = "";
+    onoff.parentElement.className = 
+     onoff.parentElement.className.replace(" inactive", "");
+    // 3. responsive margins if screen < 770 add else remove
+    if (!manpageDiv.id) { manpageDiv.id = "activeManDiv"; }
+    if (!manpageMenu.id) { manpageMenu.id = "activeManMenu"; }
+    // Turn button on
+    onOffButton = 1;
+  } else {
+    // 1.set switch, hide menu, change style, change text
+    manpageOptionMenu.dataset.toSide = 1;
+    curData.onoff = 0;
+    showItem.style.display = "none";
+    onoff.style.background = "";
+    onoff.style.color = "";
+    onoff.innerHTML = onoff.innerHTML.replace("Hide", "Show");
+    // 2. set to side when menu out of view, styling parrent with css.
+    onoff.className = "searchButtonToSide";
+    onoff.parentElement.className += " inactive";
+    // 3. responsive margins if screen < 770 add else remove
+    if (manpageDiv.id) { manpageDiv.removeAttribute("id"); }
+    if (manpageMenu.id) { manpageMenu.removeAttribute("id"); }
+    // put back to side if scrolled past 100.
     // Turn button off
     onOffButton = 0;
   }
@@ -171,45 +164,56 @@ function addOptionLinks() {
   let optionMenuLi = optionMenu.getElementsByTagName("li");
   let fullOptionList = document.getElementById("fullOptionList");
   
+  // Define var for function use.
   var lastDec, hValue;
-  var outPeriod = function() { // remove any periods    
-    if (hValue.match(/\w\d[.]/g)) {
-      lastDec = hValue.lastIndexOf(".");
-      hValue = hValue.substr(0, lastDec) + hValue.substr(Number(lastDec + 1));
+  
+  // Remove any periods from anchor link.  
+  var outPeriod = function() {    
+    let hasPeriod = 1; // period still in text
+    while (hasPeriod == 1) { // remove periods from text
+      if (hValue.substr(hValue.indexOf("#")).lastIndexOf(".") > -1) {
+        lastDec = hValue.lastIndexOf("."); // index of last period
+        // Extract to last period, skip and extract rest of text.
+        hValue = hValue.substr(0, lastDec) + 
+         hValue.substr(Number(lastDec + 1));
+       } else {
+        hasPeriod = 0; // periods have been removed
+       }
      }
   };
-  
+  // Always Show the ootion menu list.
   fullOptionList.style.display = "";
+  
+  // Select all list items in option menu
   optionMenuLi = fullOptionList.getElementsByTagName("li");
   
-  for (i = 0; i < optionMenuLi.length; i++) {
-    optionMenuLi[i].getElementsByTagName("a")[0].addEventListener("click", function() {
-      if (this.innerHTML.indexOf(",") > -1) {        
+  for (let i = 0; i < optionMenuLi.length; i++) {
+    // Add anchor link whenever list item is clicked.
+    optionMenuLi[i].getElementsByTagName("a")[0]
+    .addEventListener("click", function() {
+      if (this.innerHTML.indexOf(",") > -1) {
+        // Set the attribute value for anchor link with duplicate options.
         this.href = "#" + this.innerHTML.substr(0, this.innerHTML.indexOf(","));
-        hValue = this.href;        
+        // Duplicate value in variable to check period.
+        hValue = this.href;
       } else {
+        // Set the attribute value for anchor link.
         this.href = "#" + this.innerHTML;
+        // Duplicate value in variable to check period.
         hValue = this.href;              
       }
-      outPeriod();
-      
-      if (hValue.indexOf("#-#") > -1 || hValue.indexOf("#-:") > -1) {
-        if (hValue.indexOf("#-#") > -1) {
-          hValue = hValue.replace("#-#", "#-hash");
-        }
-        else {
-          let skip;
-        }
-        this.href = hValue;
-      } else {
-        this.href = hValue;
+      // Remove periods if has period in text
+      if (hValue.match(/\w\d[.]/g)) {
+        outPeriod();
+        this.href = hValue; // redefine attribute
       }
     });
   }
 }
-showTop();
+showTopButtonInFull();
 addOptionLinks();
 
+// Everything below is for testing and can be deleted.
 
 /***********************************************************************************
 ******************* Check console output to see results of test ********************
@@ -222,38 +226,107 @@ to turn on tests. In order to test the browser must support console.log.
      4. The results will be as such: 
        A. Anchor Count = number of options in menu 
        B. Option Anchors = array with name of options in menu 
+       C. Anchor Links = the href attribute assigned after click event.
+       D. Test Result - Pass or fail
+          - Pass - all options have href that matche innerHTML that made anchor.
+          - Note - if putting an item such as <li><a href="abc">xyz</a></li>
+                   outside of ul tag, test will fail.
+       E. Compare Values - the href value vs the innerHTML of option.       
      5. Delete or set variable "testOptionAnchors" back to 0 to turn off test.
 ***********************************************************************************/
-var testOptionAnchors = 0;
+var testOptionAnchors = 0; // test is off by default
 
 function testTheOptionAnchors() {
   var logSplit = function(x) {
     if (x == undefined) { x = 0; }
-    if (x == 2) {             
+    if (x == 1) {             
       console.log("***********************************************************************"); 
       console.log("****************************STARTING TEST******************************"); 
       console.log("\n"); 
-    } else { 
+    } 
+    else if (x == 2) { 
       console.log("\n"); 
       console.log("****************************FINISHED TEST******************************");       
-      console.log("***********************************************************************");       
-    }
+      console.log("***********************************************************************");
+    } else {
+      console.log("***********************************************************************");
+    }    
   };
-  
+
   var theUnorderdOptionList = document.getElementById("optionMenu");
   var theOptionListItems = theUnorderdOptionList.getElementsByTagName("li");  
-  var optionAnchors = [];
+  var optionListItemInnerHTML = [];
   // Get option names
-  for (i = 0; i < theOptionListItems.length; i++) {
+  for (let i = 0; i < theOptionListItems.length; i++) {
     let curItem = theOptionListItems[i].getElementsByTagName("a")[0];
-    optionAnchors.push(curItem.innerHTML);
+    optionListItemInnerHTML.push(curItem.innerHTML);    
   }
-  // output anchors
-  logSplit(2); 
+  // Get option href value.
+  var curIndex; var anchorHref = [];
+  // click then get href
+  var clickThenHref = function(cond) {
+    curIndex = 0;
+    while (curIndex < theOptionListItems.length) {
+      let curItem = theOptionListItems[curIndex].getElementsByTagName("a")[0];    
+      clickOption(curItem, cond);
+      curIndex++;
+    } 
+  };
+  // click or href.
+  var clickOption = function(cur, cond) {
+    if (cond == 1) {      
+      cur.click();      
+    } else {
+      anchorHref.push(cur.href);    
+    }    
+  };
+  // Click to add href value.
+  clickThenHref(1);
+  // Add href value to array anchorHref.
+  clickThenHref(2);
+
+  // Compare innerHTML to href.
+  var testResult = 0; var compareValues = [];  
+  if (optionListItemInnerHTML.length == anchorHref.length) {
+    let commaRegEx = /^(.*?)(?:,|$)/; // text before comma    
+    for (let i = 0; i < optionListItemInnerHTML.length; i++) {      
+      let compareLines = optionListItemInnerHTML[i].match(commaRegEx)[1].trim().replace(/\./g, "");      
+      let curAnchorValue = anchorHref[i].substr(anchorHref[i].indexOf("#")+1);      
+      
+      if (compareLines == curAnchorValue) {
+        compareValues.push(compareLines + " <-> " + curAnchorValue);
+        if (i == optionListItemInnerHTML.length-1) {
+          testResult = "PASS";
+        }
+      } else {
+        alert("ERROR - tThe href attribute did not match option in innerHTML.");
+        testResult = "FAIL";
+        break;
+      }      
+    }
+  } else {
+    alert("ERROR - the option did not generate an anchor.");
+    testResult = "FAIL";
+  }
+  
+  // Start test console composition.
+  logSplit(1); 
+  // Output option list item length and innerHTML
   console.log("A. Anchor Count - " + theOptionListItems.length);  
   console.log("B. Option Anchors:");
-  console.log(optionAnchors);  
-  logSplit();    
+  console.log(optionListItemInnerHTML);  
+  // Output option href attribute
+  logSplit();
+  console.log("C. Anchor Links:");
+  console.log(anchorHref);
+  // Make sure both lengths match and anchor uses correct option.
+  logSplit();
+  console.log("D. Test Result - ");
+  console.log(testResult);
+  console.log("E. Compare Values:");
+  console.log(compareValues);
+  // Close test console composition.
+  logSplit(2);    
 }
 
 if (testOptionAnchors == 1) { testTheOptionAnchors(); }
