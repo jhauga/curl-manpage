8073b89690d32369129f4038674df3ab03334375
John Haugabook <johnhaugabook@gmail.com>
2023-08-26 19:54:39 -0400

    remove aliases in build-test
    Removed bash aliases that were used to build and serve site locally.

--- build-test.sh
+++ build-test.sh
@@ -1,47 +1,51 @@
 #! /bin/bash
 
-echo Update curl-www repo? \[Y\\n\\Yb\]
+# These were (pretty much) the steps to test the local builds for each OS.
+
+# Update local repo with repo in feature request pull.
+echo Update curl-www repo? \[Y \\ n\]
 echo
 read _updateCurlWWW
 
-function _clonePath() {
-	if [ "$1" = "curl" ]; then
-		cd /usr/local/lib/
-		_clone https://github.com/curl/curl.git
-        cd curl
-        autoreconf -fi
-		./configure --without-ssl
-		make
-	else
-		echo The syntax is incorrect or the repo is not defined in function..
-	fi
-}
-
-if [ "$_updateCurlWWW" = "Y" ] || [ "$_updateCurlWWW" = "Yb" ]; then
-	if [ "$_updateCurlWWW" = "Yb" ]; then
-		rm -rf curl-www && git clone -b final-pre-pull https://github.com/jhauga/curl-www.git
-	else
-		rm -rf curl-www && git clone https://github.com/jhauga/curl-www.git
-	fi
+# If yes clone repo jhauga/curl-www.git.
+if [ "$_updateCurlWWW" = "Y" ]; then
+	rm -rf curl-www && git clone https://github.com/jhauga/curl-www.git
 fi
 
-echo Update curl repo for curl-www build? \[Y\\n] && echo 
+# If curl repo from site or github needs to be updated.
+echo Update curl repo for curl-www build? \[Y \\ n] && echo 
 read _updateCurl
 
+# Update by removing, cloning, then installing curl from github.
+# Although these are the steps, this was done manully on some OS.
 if [ "$_updateCurl" = "Y" ]; then 
 	echo Updating and rebuilding curl.
 	echo -----------------------------
 	echo
-	bash -i -c "_clonePath \"curl\""
+	rm -rf /usr/local/lib/curl && git clone https://github.com/curl/curl.git
+ 	cd curl
+  	autoreconf -fi
+   	./configure --without-ssl
+    	make
 fi
 
-if [ -e "_built-curl-www" ]; then
-	rm -rf _built-curl-www
+# Ask to rebuild and serve local version of site.
+echo Build local version of repo? \[Y \\ n\]
+echo
+read _buildLocalCurlWWW
+if [ "$_buildLocalCurlWWW" = "Y" ]; then
+	# remove build directory if exists.
+	if [ -e "_built-curl-www" ]; then
+		rm -rf _built-curl-www
+	fi
+ 	# copy clone to new directory to build
+	cp -r curl-www _built-curl-www
+
+ 	# build local site in build new build directory
+	cd _built-curl-www
+	echo /usr/local/lib/curl/ | sh bootstrap.sh
+	make all
+
+ 	# serve site locally
+	php -S localhost:8000
 fi
-cp -r curl-www _built-curl-www
-
-cd _built-curl-www
-echo /usr/local/lib/curl/ | sh bootstrap.sh
-make all
-
-php -S localhost:8000
